de45a19c41341ad5cc737f6e4aa1216d2976563d
NorrinRadd <Epicever94@gmail.com>
2022-09-04 21:52:58 +0300

    решение с блоксхемы(двумерный массив)

---
+++ CodeBlockshem/Program.cs
@@ -0,0 +1,37 @@
+﻿//решение с БлокСхемы(двумерный массив)
+int m = InputNumbers("Введите количество строк: ");
+int n = InputNumbers("Введите количество столбцов: ");
+int range = InputNumbers("Введите диапозон чисел от 1 до: ");
+int[,] array = new int[m,n];
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+CreateArray(array);
+WriteArray(array);
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
